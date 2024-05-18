269315e09f13dbe2e636a1bd05eb8201ae330ebc
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2024-05-18 11:17:10 +0530

    Add lenis smooth scroll to homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,6 +4,16 @@ import Meetups from '../components/Meetups';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Contact from '../components/Contact';
+import Lenis from 'lenis';
+
+// Initialize Lenis for smooth scrolling
+const lenis = new Lenis();
+function raf(time: any) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 const Homepage = () => {
 	return (
